b80f721d2baa4bde038f574e88b36c2eb90303da
Damascan-Alex <doxvirtual@gmail.com>
2020-10-24 19:36:28 +0700

    feat: finish brakets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let open = objectify(bracketsConfig);
+    let closed = objectifyBoolean(bracketsConfig);
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (open[char]) {
+            stack.push(char);
+        } else if (closed[char]) {
+            if (open[stack.pop()] !== char) return false;
+        }
+    }
+
+    return stack.length === 0;
+};
+
+// Transform from array to objects
+function objectify(array) {
+    return array.reduce(function (p, c) {
+        p[c[0]] = c[1];
+        return p;
+    }, {});
+}
+
+function objectifyBoolean(array) {
+    return array.reduce(function (p, c) {
+        p[c[1]] = true;
+        return p;
+    }, {});
 }
